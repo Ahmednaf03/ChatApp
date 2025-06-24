384937352852267fcf58ce925da4dd7d68336670
Ahmednaf03 <ahamednafees777@gamil.com>
2025-06-24 15:03:41 +0530

    router has set up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <button className='btn btn-secondary  '>daisy</button>
+    <div>
+        <Navbar/>
+        <Routes>
+           <Route path='/' element={<HomePage/>}/>
+           <Route path='/signup' element={<SignupPage />}/>
+           <Route path='/login' element={<LoginPage/>}/>
+           <Route path='/settings' element={<SettingsPage/>}/>
+           <Route path='/profile' element={<ProfilePage/>}/> 
+        </Routes>
+    </div>
   )
 }
 
